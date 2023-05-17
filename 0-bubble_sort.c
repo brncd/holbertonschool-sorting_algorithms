ecf405f29e9d667ddeaae4181a39fe110b6f1ef3
brncd <brunocarnales@gmail.com>
2023-05-17 16:07:44 -0700

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,16 +17,13 @@ void bubble_sort(int *array, size_t size)
 		swapped = 0;
 		for (i = 0; i < size; i++)
 		{
-			if (array[i] > array[i + 1])
+			if ((array[i] > array[i + 1]) && if ((i + 1) < size))
 			{
-				if ((i + 1) < size)
-				{
-					temp = array[i + 1];
-					array[i + 1] = array[i];
-					array[i] = temp;
-					swapped += 1;
-					print_array(array, size);
-				}
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				swapped += 1;
+				print_array(array, size);
 			}
 		}
 
