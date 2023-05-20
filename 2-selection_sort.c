673bb5db2d25e66f7167b8a73de8aef568dcaf8e
brncd <brunocarnales@gmail.com>
2023-05-19 20:55:18 -0700

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * @arr: Array to be sorted
+ * @n: Size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j, min_idx, tmp;
+
+	while (i < (size - 1))
+	{
+		min_idx = i;
+		j = i + 1;
+
+		while (j < size)
+		{
+			if (array[min_idx] > array[j])
+				min_idx = j;
+			j++;
+		}
+
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+
+		i++;
+	}
+}
