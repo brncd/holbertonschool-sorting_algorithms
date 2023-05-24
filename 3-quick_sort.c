892ad18df49b5975314d4ae28c44164afaa8a476
alejandr088 <alejandrivello@gmail.com>
2023-05-24 15:55:09 -0700

    fixed function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,16 +3,14 @@
 /**
  * swap - Swaps two integers
  *
- * @a: Pointer to first integer
- * @b: Pointer to second integer
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
  */
 void swap(int *a, int *b)
 {
-	int temp;
-    
-    temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /**
@@ -26,24 +24,23 @@ void swap(int *a, int *b)
  */
 int partition(int *array, int low, int high)
 {
-	int pivot, i, j;
+    int pivot, i, j;
 
+    i = low - 1;
     pivot = array[high];
-	i = low - 1;
+    
 
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, high + 1);
-		}
-	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, high + 1);
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
 
-	return (i + 1);
+    return (i + 1);
 }
 
 /**
@@ -55,15 +52,13 @@ int partition(int *array, int low, int high)
  */
 void quicksort_recursive(int *array, int low, int high)
 {
-	if (low < high)
-	{
-		int pivot;
-        
-        pivot = partition(array, low, high);
+    if (low < high)
+    {
+        int pivot = partition(array, low, high);
 
-		quicksort_recursive(array, low, pivot);
-		quicksort_recursive(array, pivot + 1, high);
-	}
+        quicksort_recursive(array, low, pivot - 1);
+        quicksort_recursive(array, pivot + 1, high);
+    }
 }
 
 /**
@@ -74,8 +69,8 @@ void quicksort_recursive(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	quicksort_recursive(array, 0, size - 1);
+    quicksort_recursive(array, 0, size - 1);
 }
